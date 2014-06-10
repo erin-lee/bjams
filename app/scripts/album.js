b46b5a8d98ea9e1462acffdd67da5dee46738940
erin <erinlee2007@u.northwestern.edu>
2014-06-10 04:21:10 +0000

    used jquery to populate the album view.

---
+++ app/scripts/album.js
@@ -0,0 +1,57 @@
+var createSongRow = function(songNumber, songName, songLength) {
+  var template =
+      '<tr>'
+    + '  <td class="col-md-1">' + songNumber + '</td>'
+    + '  <td class="col-md-9">' + songName + '</td>'
+    + '  <td class="col-md-2">' + songLength + '</td>'
+    + '</tr>'
+    ;
+
+  return $(template);
+};
+
+// Example Album
+var albumMarconi = {
+  name: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: '/images/album-placeholder.png',
+
+  songs: [
+      { name: 'Hello, Operator?', length: '1:01' },
+      { name: 'Ring, ring, ring', length: '5:01' },
+      { name: 'Fits in your pocket', length: '3:21'},
+      { name: 'Can you hear me now?', length: '3:14' },
+      { name: 'Wrong phone number', length: '2:15'}
+    ]
+};
+
+var changeAlbumView = function(album) {
+  var $albumTitle = $('.album-title');
+  $albumTitle.text(album.name);
+
+  var $albumArtist = $('.album-artist');
+  $albumArtist.text(album.artist);
+
+  var $albumMeta = $('.album-meta-info');
+  $albumMeta.text(album.year + ' on ' + album.label);
+
+  var $songList = $('.album-song-listing');
+  $songList.empty();
+  var songs = album.songs;
+
+  for (var i = 0; i < songs.length; i++) {
+    var songData = songs[i];
+    var $newRow = createSongRow(i, songData.name, songData.length);
+    $songList.append($newRow);
+  }
+};
+
+if (document.URL.match(/\/album.html/)) {
+  document.addEventListener('DOMContentLoaded', function() {
+    var album = albumMarconi;
+
+    changeAlbumView(albumMarconi);
+  });
+}
