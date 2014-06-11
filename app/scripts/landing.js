d4b8117e29be13309f59812405259053e03fdb7c
erin <erinlee2007@u.northwestern.edu>
2014-06-11 21:17:38 +0000

    change to jquery each function

--- app/scripts/landing.js
+++ app/scripts/landing.js
@@ -1,24 +1,23 @@
 $('document').ready('DOMContentLoaded', function() {
-  var heroTitle
+  var $heroTitle
   , subText
-  , sellPoint = document.querySelectorAll('.selling-points .point');
+  , $this = $(this)
+  , $sellPoint = $('.selling-points .point')
+  ;
 
-  heroTitle = document.querySelector('.hero-content h3');
+  $heroTitle = $('.hero-content h3');
   heroTitle.addEventListener('click', function() {
-    subText = this.textContent;
-    this.textContent = subText + '!';
+    subText = $this.textContent;
+    $this.textContent = subText + '!';
   });
   var onHoverAction = function(e) {
-    this.style.marginTop = '10px';
+    $this.style.marginTop = '10px';
   };
 
   var offHoverAction = function(e) {
-    this.style.marginTop = 0;
+    $this.style.marginTop = 0;
   };
 
-  for( var x=0; x< sellPoint.length; x++) {
-    sellPoint[x].addEventListener('mouseover', onHoverAction);
-    sellPoint[x].addEventListener('mouseout', offHoverAction);
-  }
+  $sellPoint.hover(onHoverAction, offHoverAction);
 
 });
