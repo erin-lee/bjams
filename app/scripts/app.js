6d47500d9726eda6d47574d0bbd365917bd083ad
Erin L <erinlee2007@u.northwestern.edu>
2014-06-16 23:36:27 -0400

    Converted Bloc Jams to a single page application

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,21 +4,38 @@
 // require("./profile");
 
 
-var app = angular.module('blocJams', []);
+var app = angular.module('blocJams', ['ui.router']);
 
-app.controller('LandingController', function() {
-  this.subText = "Turn the music up!";
-  this.title = "Bloc Jams";
-  this.subTextClicked = function() {
-    return this.subText += '!';
+app.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'LandingController',
+    templateUrl: '/templates/landing.html'
+  });
+
+  $stateProvider.state('song', {
+    url: '/song',
+    // controller: 'LandingController',
+    templateUrl: '/templates/song.html'
+  });
+}]);
+
+app.controller('LandingController', ['$scope', function($scope) {
+  console.log('LandingController');
+  $scope.subText = "Turn the music up!";
+  $scope.title = "Bloc Jams";
+  $scope.subTextClicked = function() {
+    return $scope.subText += '!';
   };
 
-  this.shuffle = function(o) {
+  $scope.shuffle = function(o) {
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
   };
 
-  this.albumUrls = [
+  $scope.albumUrls = [
     '/images/album-placeholders/album-1.jpg',
     '/images/album-placeholders/album-2.jpg',
     '/images/album-placeholders/album-3.jpg',
@@ -29,4 +46,4 @@ app.controller('LandingController', function() {
     '/images/album-placeholders/album-8.jpg',
     '/images/album-placeholders/album-9.jpg',
   ];
-});
+}]);
