87597a288867f810dec5c6b05f7925e9837c2d0e
Erin L <erinlee2007@u.northwestern.edu>
2014-06-16 22:30:57 -0400

    Converted landing page to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,32 @@
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+// require("./landing");
+// require("./collection");
+// require("./album");
+// require("./profile");
+
+
+var app = angular.module('blocJams', []);
+
+app.controller('LandingController', function() {
+  this.subText = "Turn the music up!";
+  this.title = "Bloc Jams";
+  this.subTextClicked = function() {
+    return this.subText += '!';
+  };
+
+  this.shuffle = function(o) {
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+  this.albumUrls = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg',
+  ];
+});
