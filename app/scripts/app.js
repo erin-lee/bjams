b1b724b5b31ce9e51c39408e52f5cd29d5c53129
Erin L <erinlee2007@u.northwestern.edu>
2014-06-21 17:33:58 -0400

    Made next and previous buttons on player-bar work.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -135,6 +135,10 @@ app.service('ConsoleLogger', function() {
 });
 
 app.service('SongPlayer', function(){
+  var trackIndex = function(album, song) {
+    return album.songs.indexOf(song);
+  };
+
   return {
     currentSong: null,
     currentAlbum: null,
@@ -145,6 +149,22 @@ app.service('SongPlayer', function(){
     },
     pause: function() {
       this.playing = false;
+    },
+    next: function() {
+      var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+      currentTrackIndex++;
+      if (currentTrackIndex >= this.currentAlbum.songs.length) {
+        currentTrackIndex = 0;
+      }
+      this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+    },
+    previous: function() {
+      var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+      currentTrackIndex--;
+      if (currentTrackIndex < 0) {
+        currentTrackIndex = this.currentAlbum.songs.length - 1;
+      }
+      this.currentSong = this.currentAlbum.songs[currentTrackIndex];
     },
     setSong: function(album, song) {
       this.currentAlbum = album;
