0b49d404edc1568fdefb973265bb54b3e29d1256
Erin L <erinlee2007@u.northwestern.edu>
2014-06-26 23:37:42 -0400

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -121,6 +121,18 @@ app.controller('AlbumController', ['$scope', 'SongPlayer', 'ConsoleLogger',funct
 app.controller('PlayerBarController', ['$scope', 'SongPlayer', 'ConsoleLogger', function($scope, SongPlayer, ConsoleLogger) {
   $scope.songPlayer = SongPlayer;
 
+  $scope.volumeClass = function() {
+    return {
+      'fa-volume-off': SongPlayer.volume == 0,
+      'fa-volume-down' : SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+      'fa-volume-up' : SongPlayer.volume > 70
+    };
+  };
+  $scope.muteVolume = function() {
+    if (SongPlayer.volume > 0) {
+      SongPlayer.volume = 0;
+    };
+  };
   SongPlayer.onTimeUpdate(function(event, time) {
     $scope.$apply(function(){
       $scope.playTime = time;
@@ -147,6 +159,7 @@ app.service('SongPlayer', function($rootScope){
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
 
     play: function() {
       this.playing = true;
@@ -182,6 +195,12 @@ app.service('SongPlayer', function($rootScope){
     onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    setVolume: function(volume) {
+      if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -192,6 +211,8 @@ app.service('SongPlayer', function($rootScope){
         formats: [ "mp3" ],
         preload: true
       });
+
+      currentSoundFile.setVolume(this.volume);
       currentSoundFile.bind('timeupdate', function(e) {
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
       });
